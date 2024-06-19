c9df1f010f161e7cfd2d05cd8bf4aa2dd200dc9d
Pedrada66 <pedro.coppo@yahoo.com.br>
2024-06-19 00:48:58 -0300

    Adicionar Módulo View com Telas
    Módulo View com maioria das telas prontas

---
+++ NuBonk/src/main/java/com/mycompany/nubonk/NuBonk.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nubonk;
+import View.TelaInicial;
+/**
+ *
+ * @author pedro
+ */
+public class NuBonk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        TelaInicial tp = new TelaInicial();
+        tp.setVisible(true);
+    }
+}
